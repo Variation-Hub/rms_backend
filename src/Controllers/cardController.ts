a504a9a7f4d31634eb3cee1c9e148f3045a07a53
jeel <jeeltadhani3@gmail.com>
2024-08-04 19:13:18 +0530

    initial commit

--- src/Controllers/cardController.ts
+++ src/Controllers/cardController.ts
@@ -10,7 +10,7 @@ export const createCard = async (req: any, res: Response) => {
             file = await uploadToBackblazeB2(req.file, "card")
         }
         const card = await CardModel.create({ ...req.body, file })
-        emailHelper("jeeltadhani3@gmail.com", card)
+        emailHelper("Abhishek@weetgateithub.com", card)
 
         return res.status(200).json({
             message: "card create success",
